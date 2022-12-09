2212df41554442249a1199c4c3c7a1fe28dae67d
Misha Panivnyk <panivnykm@gmail.com>
2022-12-09 22:52:09 +0200

    add api

---
+++ src/services/api.js
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types';
+import axios from 'axios';
+export default function fetchImagesWithQuery(searchQuery, page) {
+  const response = axios.get(
+    `https://pixabay.com/api/?q=${searchQuery}&page=${page}&key=30575262-802bd827513eacead1493e6f3&image_type=photo&orientation=horizontal&per_page=12`
+  );
+  return response;
+}
+fetchImagesWithQuery.propTypes = {
+  searchQuery: PropTypes.string.isRequired,
+  page: PropTypes.number.isRequired,
+};
